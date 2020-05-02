c6399c1aa94053ce1cd61362b39bfd169f0125de
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-05-02 18:13:31 +0200

    Refactor RestaurantList tests

--- src/components/__tests__/RestaurantList.spec.jsx
+++ src/components/__tests__/RestaurantList.spec.jsx
@@ -3,28 +3,34 @@ import {render} from '@testing-library/react';
 import {RestaurantList} from '../RestaurantList';
 
 describe('RestaurantList', () => {
-  it('loads restaurants on first render', () => {
-    // Arange: it will ask to load Restaurants
-    const loadResto = jest.fn().mockName('loadRestaurants');
+  // constants
+  const sushiPlace = 'Sushi Place';
+  const saladPlace = 'Salad Place';
+  const restaurants = [
+    {id: 1, name: sushiPlace},
+    {id: 2, name: saladPlace},
+  ];
+  let loadResto;
+  let context;
+
+  beforeEach(() => {
+    loadResto = jest.fn().mockName('loadRestaurants');
     // Act: render component
-    render(<RestaurantList loadRestaurants={loadResto} restaurants={[]} />);
+    context = render(
+      <RestaurantList loadRestaurants={loadResto} restaurants={restaurants} />,
+    );
+  });
+
+  it('loads restaurants on first render', () => {
     // Assert: check the function was called
     expect(loadResto).toHaveBeenCalled();
   });
 
   it('displays restaurants', () => {
-    // we don't test loadRestaurant function here so a noop fits
-    const noop = () => {};
-    const restaurants = [
-      {id: 1, name: 'Sushi Place'},
-      {id: 2, name: 'Salad Place'},
-    ];
     // Act
-    const {queryByText} = render(
-      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
-    );
+    const {queryByText} = context;
     // Assert: find an element containing the passed-in text if not found returns null
-    expect(queryByText('Sushi Place')).not.toBeNull();
-    expect(queryByText('Salad Place')).not.toBeNull();
+    expect(queryByText(sushiPlace)).not.toBeNull();
+    expect(queryByText(saladPlace)).not.toBeNull();
   });
 });
