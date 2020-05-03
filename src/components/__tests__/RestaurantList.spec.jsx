fba95f9c21ffdca299b7af966180fbebbe9ed7bb
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-05-03 17:18:43 +0200

    refactor RestaurantList tests to pass props

--- src/components/__tests__/RestaurantList.spec.jsx
+++ src/components/__tests__/RestaurantList.spec.jsx
@@ -10,27 +10,37 @@ describe('RestaurantList', () => {
     {id: 1, name: sushiPlace},
     {id: 2, name: saladPlace},
   ];
-  let loadResto;
+  let loadRestaurants;
   let context;
 
-  beforeEach(() => {
-    loadResto = jest.fn().mockName('loadRestaurants');
-    // Act: render component
-    context = render(
-      <RestaurantList loadRestaurants={loadResto} restaurants={restaurants} />,
-    );
-  });
+  const renderWithProps = (propsOverrides = {}) => {
+    const props = {
+      loadRestaurants: jest.fn().mockName('loadRestaurants'),
+      restaurants,
+      ...propsOverrides,
+    };
+    loadRestaurants = props.loadRestaurants;
+    context = render(<RestaurantList {...props} />);
+  };
 
   it('loads restaurants on first render', () => {
+    renderWithProps();
     // Assert: check the function was called
-    expect(loadResto).toHaveBeenCalled();
+    expect(loadRestaurants).toHaveBeenCalled();
   });
 
   it('displays restaurants', () => {
     // Act
+    renderWithProps();
     const {queryByText} = context;
     // Assert: find an element containing the passed-in text if not found returns null
     expect(queryByText(sushiPlace)).not.toBeNull();
     expect(queryByText(saladPlace)).not.toBeNull();
+  });
+
+  it('displays the loading indicator while loading', () => {
+    renderWithProps({loading: true});
+    const {queryByTestId} = context;
+    expect(queryByTestId('loading-indicator')).not.toBeNull();
   });
 });
