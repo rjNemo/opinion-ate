fc1a13fd86fffb60e4b48f74b2093952a2b7d1b3
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-05-03 16:37:09 +0200

    style with material ui

--- src/components/RestaurantScreen.jsx
+++ src/components/RestaurantScreen.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import RestaurantList from './RestaurantList';
+import {makeStyles} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const RestaurantScreen = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Restaurants</h1>
-      <RestaurantList />
-    </div>
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography variant="h5">Restaurants</Typography>
+        <RestaurantList />
+      </CardContent>
+    </Card>
   );
 };
 export default RestaurantScreen;
