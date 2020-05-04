e5d317797a421efbb5a8b53d8606da09265a45c6
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-05-04 15:24:20 +0200

    refactor UI

--- src/components/NewRestaurantForm.jsx
+++ src/components/NewRestaurantForm.jsx
@@ -1,15 +1,15 @@
 import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import {TextField, Button, makeStyles} from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 import Alert from '@material-ui/lab/Alert';
 import {createRestaurant} from '../store/restaurants/actions';
 
 const useStyles = makeStyles(theme => ({
-  form: {
-    marginTop: '1rem',
-  },
-  button: {
-    margin: '1rem 0',
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
   },
 }));
 
@@ -38,30 +38,31 @@ export const NewRestaurantForm = ({createRestaurant}) => {
   const handleChange = e => setName(e.target.value);
 
   return (
-    <form className={classes.form} onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
       {serverError && (
         <Alert severity="error">
           The restaurant could not be saved. Please try again.
         </Alert>
       )}
       {validationError && <Alert severity="error">Name is required</Alert>}
-      <TextField
-        placeholder="Add Restaurant"
-        value={name}
-        onChange={handleChange}
-        fullWidth
-        variant="outlined"
-        // label="Add Restaurant"
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        type="submit"
-        className={classes.button}
-        data-testid="new-restaurant-submit-button"
-      >
-        Add
-      </Button>
+      <Box display="flex" className={classes.root}>
+        <TextField
+          placeholder="Add Restaurant"
+          value={name}
+          onChange={handleChange}
+          fullWidth
+          variant="outlined"
+          // label="Add Restaurant"
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          data-testid="new-restaurant-submit-button"
+        >
+          Add
+        </Button>
+      </Box>
     </form>
   );
 };
